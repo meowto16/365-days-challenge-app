95341cf36bf0d382f0b0095c6e1698abbd0f751c
Maxim Kirshin <meowto16@gmail.com>
2022-05-11 23:09:52 +0500

    Test service worker #2

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,7 @@
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+)
+
 const CACHE_NAME = '365-days-challenge-app-version-1.0.0'
 
 const urlsToCache = ['/']
@@ -12,16 +16,16 @@ self.addEventListener('install', (event) => {
 })
 
 // Cache and return requests
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      if (response) {
-        return response
-      }
-      return fetch(event.request)
-    })
-  )
-})
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then(function (response) {
+//       if (response) {
+//         return response
+//       }
+//       return fetch(event.request)
+//     })
+//   )
+// })
 
 // Update a service worker
 self.addEventListener('activate', (event) => {
