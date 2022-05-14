040cd88e2748413b751e920de9997cb4b7f1f55f
Maxim Kirshin <meowto16@gmail.com>
2022-05-14 18:52:52 +0500

    feature: makes app pwa with cache

--- public/sw.js
+++ public/sw.js
@@ -1,31 +1,49 @@
 const CACHE_NAME = '365-days-challenge-app-version-1.0.1'
 
-const urlsToCache = ['/']
+const urlsToCache = [
+  '/',
+  '/js/script.js',
+  '/images/maxim-kirshin.jpeg',
+  '/images/evgeniy-razboynikov.jpeg',
+  '/images/ruslan-bekshenev.jpg',
+  '/images/maxim-petrov.jpeg',
+  '/svg/sprite.svg',
+  '/fonts/Montserrat/Montserrat-Bold.ttf',
+  '/fonts/Montserrat/Montserrat-Regular.ttf',
+  '/manifest.json',
+  '/icon-192x192.png',
+  '/favicon.ico',
+  '/vendor/popper.min.js',
+  '/vendor/pulltorefresh.min.js',
+  '/css/style.css'
+]
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log('Opened cache')
       return cache.addAll(urlsToCache)
     })
   )
 })
 
 // Cache and return requests
-// self.addEventListener('fetch', (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then(function (response) {
-//       if (response) {
-//         return response
-//       }
-//       return fetch(event.request)
-//     })
-//   )
-// })
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        console.log('[MATCHED]: ' + event.request.url)
+        return response
+      }
+      console.log('[NOT MATCHED]: ' + event.request.url)
+      return fetch(event.request)
+    })
+  )
+})
 
 // Update a service worker
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = ['your-app-name']
+  const cacheWhitelist = [CACHE_NAME]
+
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
