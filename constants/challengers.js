84b4fe01d27d2616a407916e7562230580547ef6
Maxim Kirshin <meowto16@gmail.com>
2023-01-02 15:29:46 +0500

    Changes challengers for 2023 year

--- constants/challengers.js
+++ constants/challengers.js
@@ -15,75 +15,7 @@ const challengers = [
       birthday: new Date('2022-10-28')
     },
     {
-      startDate: new Date('2022-01-01')
-    }
-  ),
-  new Challenger(
-    'e-razboinikov',
-    '@e.razboinikov',
-    {
-      name: 'Razboinikov E.',
-      avatar: '/images/evgeniy-razboynikov.jpeg',
-      specialization: 'flutter',
-      socials: {
-        vk: 'https://vk.com/e.razboinikov',
-        github: 'https://github.com/e-razboinikov'
-      },
-      birthday: new Date('2022-05-22')
-    },
-    {
-      startDate: new Date('2022-01-01')
-    }
-  ),
-  new Challenger(
-    'ruslan-bekshenev',
-    '@mynameisruslanbek',
-    {
-      name: 'Bekshenev R.',
-      avatar: '/images/ruslan-bekshenev.jpg',
-      specialization: 'react',
-      socials: {
-        vk: 'https://vk.com/mynameisruslanbek',
-        github: 'https://github.com/ruslan-bekshenev'
-      },
-      birthday: new Date('2022-01-23')
-    },
-    {
-      startDate: new Date('2022-01-21')
-    }
-  ),
-  new Challenger(
-    'maq7p',
-    '@maqpug',
-    {
-      name: 'Petrov M.',
-      avatar: '/images/maxim-petrov.jpeg',
-      specialization: 'react',
-      socials: {
-        vk: 'https://vk.com/maqpug',
-        github: 'https://github.com/maq7p'
-      },
-      birthday: new Date('2022-03-07')
-    },
-    {
-      startDate: new Date('2022-01-21')
-    }
-  ),
-  new Challenger(
-    'masterdensan',
-    '@nightshikiden',
-    {
-      name: 'Lambin D.',
-      avatar: '/images/denis-lambin.jpeg',
-      specialization: 'react',
-      socials: {
-        vk: 'https://vk.com/nightshikiden',
-        github: 'https://github.com/MasterDenSan'
-      },
-      birthday: new Date('2022-08-08')
-    },
-    {
-      startDate: new Date('2022-07-18')
+      startDate: new Date('2023-01-01')
     }
   )
 ]
